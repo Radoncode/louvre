8df455ef1d1a1a134ef885ecace43579e082bc28
radoncode <relbouni69@gmail.com>
2018-02-24 19:20:01 +0100

    creating a new booking form with the date

---
+++ src/LP/ReservationBundle/Form/ReservationType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace LP\ReservationBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReservationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+             ->add('type',ChoiceType::class,array('label'=>'Type de billet (Plus de billet journée disponible après 14H)',
+                                                              'choices'=>array("Billet Journée (jusqu'à 14H)"=> 'billetjournee',
+                                                                               'Billet demi-journée (à partir de 14H)'=>'demijournee'),
+                                                                               'choice_attr'=> function($val){
+                                                                                                                return['id'=>$val];
+                                                                                                                }))
+             ->add('name',TextType::class,array('label'=>'Prénom'))
+             ->add('firstname',TextType::class,array('label'=>'Nom'))
+
+             ->add('birthday',DateType::class,array('label'=>'Date de naissance',
+                                                                'widget'=>'single_text',
+                                                                'html5'=>false,
+                                                                'format'=>'dd/MM/yy',
+                                                                'attr'=>array('class'=>'datepicker-js')))
+
+             ->add('email',EmailType::class,array('label'=>'E-mail'))
+             ->add('country',CountryType::class,array('label'=>'Pays'))
+
+             ->add('reservationdate',Datetype::class,array('label'=>'Jour de la Visite',
+                                                                       'widget'=>'single_text',
+                                                                       'html5'=>false,
+                                                                       'format'=>'dd/MM/yy',
+                                                                       'attr'=>array('class'=>'js-datepicker')))
+             ->add('valider',SubmitType::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'LP\ReservationBundle\Entity\Reservation'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'lp_reservationbundle_reservation';
+    }
+
+
+}
